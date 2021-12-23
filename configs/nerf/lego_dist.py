c8eb8bb8d23cfe0adc023be0073ab0f7e8fc1d9e
LeoXing <leoxing1996@gmail.com>
2021-12-23 11:38:46 +0800

    add log

--- configs/nerf/lego_dist.py
+++ configs/nerf/lego_dist.py
@@ -1,1 +1,8 @@
 _base_ = ['../_base_/datasets/blender_dataset_dist.py', './lego.py']
+
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='PaviLoggerHook', init_kwargs=dict(project='NeRF'))
+    ])
